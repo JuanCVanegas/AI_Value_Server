b95c805efa01d62d7c6f25f1ab3cd62c2dab106b
JuanCVanegas <65472730+JuanCVanegas@users.noreply.github.com>
2020-06-26 17:53:52 -0500

    Initialize frontend

---
+++ frontend/ai-value-gui/src/App.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import Router from './Router'
+import './App.css';
+import { BrowserRouter } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+
+//-----------------------------
+
+// import React from "react";
+// import { Redirect, Link, Route, Switch } from "react-router-dom";
+// import Category from "./Components/Dashboard/Drivers/Category";
+// import Products from "./Components/Dashboard/Drivers/Drivers";
+// import Login, { fakeAuth } from "./Login";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <nav className="navbar navbar">
+//         <ul className="nav navbar-nav">
+//           <li>
+//             <Link to="/">Homes</Link>
+//           </li>
+//           <li>
+//             <Link to="/category">Category</Link>
+//           </li>
+//           <li>
+//             <Link to="/products">Products</Link>
+//           </li>
+//           <li>
+//             <Link to="/admin">Admin area</Link>
+//           </li>
+//         </ul>
+//       </nav>
+
+//       <Switch>
+//         <Route path="/login" component={Login} />
+//         <Route exact path="/" component={Home} />
+//         <Route path="/category" component={Category} />
+//         <PrivateRoute path="/admin" component={Admin} />
+//         <Route path="/products" component={Products} />
+//       </Switch>
+//     </div>
+//   );
+// }
+
+// /* PrivateRoute component definition */
+// const PrivateRoute = ({ component: Component, ...rest }) => {
+//   return (
+//     <Route
+//       {...rest}
+//       render={props =>
+//         fakeAuth.isAuthenticated === true ? (
+//           <Component {...props} />
+//         ) : (
+//             <Redirect
+//               to={{ pathname: "/login", state: { from: props.location } }}
+//             />
+//           )
+//       }
+//     />
+//   );
+// };
+
+// //Home component
+// const Home = props => (
+//   <div>
+//     <h2>Home {console.log(props)}</h2>
+//   </div>
+// );
+
+// //Admin component
+// const Admin = ({ match }) => {
+//   return (
+//     <div>
+//       {" "}
+//       <h2>Welcome admin </h2>
+//     </div>
+//   );
+// };
